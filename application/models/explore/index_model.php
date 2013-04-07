44a41f5ae1fc3a1b485c5a6e807a0e88ed66c154
huip <penghui1012@gmail.com>
2013-04-08 04:09:39 +0800

    修改了搜索的bug

--- application/models/explore/index_model.php
+++ application/models/explore/index_model.php
@@ -18,7 +18,10 @@ class Index_model extends CI_Model
           $sql = "SELECT us.user_img,us.user_name,us.user_id,ms.msgid,ms.post_time,ms.ques_title,ms.ques_socore,ms.browser,
           ms.is_best,'$numpage' AS num,(SELECT count(*) FROM guwen_comment WHERE comment_quesid = ms.msgid) AS anwser,
          (SELECT tag_name FROM guwen_tag WHERE id = ms.ques_cate ) AS ques_cate FROM guwen_message AS ms, guwen_user AS us 
-         WHERE ms.user_id = us.user_id ORDER BY anwser DESC,ms.post_time DESC, ms.browser DESC  LIMIT $offset,$pagesize";
+         WHERE ms.user_id = us.user_id
+          ORDER BY 
+          (anwser*0.2+ (now() -ms.post_time)*0.1 + us.user_score*0.3 +ms.is_best*100+ (ms.browser/100)*0.2 +ms.ques_socore*0.2)/
+        ((now() -ms.post_time) +anwser+ms.browser+us.user_score + ms.ques_socore)  DESC  LIMIT $offset,$pagesize";
           $query = $this->db->query($sql);
           $res = $query->result_array();
           if($offset == 0 )
