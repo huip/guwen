7d0abac6a9c58280a82aa821c4d711e0839d0ee2
huip <penghui1012@gmail.com>
2013-04-08 13:35:47 +0800

    no message

--- application/views/conn/header.php
+++ application/views/conn/header.php
@@ -3,8 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>故问</title>
-	<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>
-	<meta name="keywords" content="学生地带，中南民族大学，学生门户网站，学生资讯集团，比特工场，故问，学生工作通讯"/>
+	<meta name="keywords" content="学生地带，明知故问，中南民族大学，学生门户网站，学生资讯集团，比特工场，故问，学生工作通讯"/>
 	<meta  name="description" content="学生地带是由中南民族大学学生工作部（处）主办，学生资讯集团负责研发运营，以提升大学生活 品质为核心定位的学生问答知识社区" />
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/bootstrap.min.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/conn.css" type="text/css" />
