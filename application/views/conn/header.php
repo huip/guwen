839c7bcdcf3d82af134143e883ac60a9c67e3b95
huip <penghui1012@gmail.com>
2013-04-08 13:27:51 +0800

    no message

--- application/views/conn/header.php
+++ application/views/conn/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>故问</title>
+	<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/bootstrap-responsive.min.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/bootstrap.min.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/conn.css" type="text/css" />
