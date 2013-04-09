0539d37ca0bd76e591d71a50cd491561e972dbfc
huip <penghui1012@gmail.com>
2013-04-09 18:34:36 +0800

    no message

--- data/js/main.js
+++ data/js/main.js
@@ -1410,7 +1410,7 @@ if ( user_id != undefined ) {
 
 // return current web app's full dir
 function get_root_path() {  
-  var root = location.protocol + '//' + location.host+'/guwen/index.php';
+  var root = location.protocol + '//' + location.host+'/index.php';
   return root;
 }
 
