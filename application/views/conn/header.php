626319de0161cb569d6c586ae8bc97eb8319a882
huip <353233057@>
2013-04-12 11:00:36 +0800

    no message

--- application/views/conn/header.php
+++ application/views/conn/header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>故问</title>
 	<meta name="keywords" content="学生地带，明知故问，中南民族大学，学生门户网站，学生资讯集团，比特工场，故问，学生工作通讯"/>
-	<meta  name="description" content="学生地带是由中南民族大学学生工作部（处）主办，学生资讯集团负责研发运营，以提升大学生活 品质为核心定位的学生问答知识社区" />
+	<meta  name="description" content="故问是由中南民族大学比特工场开发，以提升大学生活, 品质为核心定位的学生问答知识社区" />
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/bootstrap.min.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/conn.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo base_url();?>data/css/jquery.Jcrop.min.css" type="text/css" />
