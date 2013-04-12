f32b5dd51e264d0fcade716d3334a682b4fe151c
huip <penghui1012@gmail.com>
2013-04-12 23:19:42 +0800

    no message

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -4,9 +4,9 @@
     <div class="span9">
       <h5 class="modal-header"><i class=" icon-search"></i>&nbsp<span>搜索结果</span></h5>
       <?foreach ($search as $key => $value): {?>
-      	<div>
+      	<div class="search-list">
       		<p><a href="<?=base_url();?>index.php/question/index/<?=$value['msgid']?>"><?=$value['ques_title']?></a></p>
-      		<p><span>共<?=$value['answer']?>个回答</span><span>&nbsp&nbsp浏览<?=$value['browser']?></span><span class="sns-time-list"><?=$value['post_time']?></span></p>
+      		<p class="reply-color"><span>共<?=$value['answer']?>个回答</span><span>&nbsp&nbsp浏览<?=$value['browser']?></span><span class="sns-time-list pull-right"><?=$value['post_time']?></span></p>
       	</div>
       <?}endforeach?>
     </div>
