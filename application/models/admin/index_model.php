2547046ec45e180ddb1837912ecdc71afbcc9c1f
huip <353233057@>
2013-04-13 02:01:46 +0800

    no message

--- application/models/admin/index_model.php
+++ application/models/admin/index_model.php
@@ -14,4 +14,27 @@ class index_model extends CI_Model
         $res = $query->result_array();
         return $res;
       }
+
+      public function clear_keywords()
+      {
+             
+              $whither_words = array(
+
+                            "一些","还是","如题","用下","价的","什么","大公","如题","这么","今天","一道","的问","我严","请问","的餐","这里","谁知"
+
+              );
+              $sql = "SELECT id,keywords FROM guwen_keywords";
+              $query = $this->db->query($sql);
+              $res = $query->result_array();
+
+              foreach ($res as $key => $value)
+              {
+                      $keywords_diff = implode(",",array_diff(explode(",", $value['keywords']),$whither_words));
+
+                      $sql = "UPDATE guwen_keywords SET keywords ='$keywords_diff'   WHERE id = ?";
+                      $query = $this->db->query($sql,$value['id']);
+              }
+
+              return $keywords_diff;
+      }
 }
