d96bb6b445ebfb46998a304570d5b0c2eb9c309b
huip <353233057@>
2013-04-28 15:00:27 +0800

    no message

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -10,7 +10,7 @@
             	<div class="feed-list span11">
             		<a href="<?=base_url();?>index.php/person/question/<?=$value['user_id']?>" ><img src="<?=$value['user_img'];?>" alt="user_logo" class="user-img span1" /></a>
             		<div class="feed-content span11">
-            			<!-- <p class="feed-content-name"><span><a class="user-name-title" href="<?=base_url()?>index.php/person/question/<?=$value['user_id']?>" ><?=$value['user_name'];?></a></span></p> -->
+            			<p class="feed-content-name"><span><a class="user-name-title" href="<?=base_url()?>index.php/person/question/<?=$value['user_id']?>" ><?=$value['user_name'];?></a></span></p>
             			<p><a href="<?=base_url()?>index.php/question/index/<?=$value['msgid'];?>"   class="title-a"><?=$value['ques_title'];?></a><span class="sns-time-list pull-right"><?=$value['post_time'];?></span></p>
 <!--                               <div class='index-content-list'><?=nl2br($value['ques_content'])?></div> -->
             			<p class="sns-bar reply-color"><span>悬赏:<?=$value['ques_socore'];?></span>
