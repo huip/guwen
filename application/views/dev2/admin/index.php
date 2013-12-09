6a799c7641a9a8cb3429756a09651acc0464e1ac
chinahgr <polandeme@gmail.com>
2013-12-09 23:00:22 +0800

    change admin.php

---
+++ application/views/dev2/admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="<?php echo base_url(); ?>/data/stylesheets/lib/bootstrap.min.css"  type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>/data/css/conn-min.css" type="text/css" />
+</head>
+<body>
+<div class = "navbar">
+    <div class = "navbar-inner">
+        <a class = "brand" href = "#"> 后台管理 </a>
+        <ul class="nav">
+            <li class = "active"><a href= "#">首页</a> <li>
+            <li><a href="#"> 后台</a></li>
+        </ul>
+    </div>
+</div>
+<!-- <script src="<?php echo base_url();?>data/javascripts/sea&#45;modules/seajs/seajs/2.1.1/sea.js"></script> -->
+<!-- <script> -->
+<!-- seajs.config({ -->
+<!--     base: "<?php echo base_url();?>data/javascripts/sea&#45;modules/", -->
+<!--     alias: { -->
+<!--         "$": "jquery/jquery/1.10.1/jquery.js" -->
+<!--     } -->
+<!-- }); -->
+<!-- seajs.use("<?php echo base_url();?>data/javascripts/admin/admin"); -->
+<!-- </script> -->
+<!-- <script src="</script> -->
+</body>
+</html>
+
---
+++ data/javascripts/admin/admin.coffee
@@ -0,0 +1,8 @@
+foo = "bar"
+outer = 1
+change = ->
+    inner = -1
+    outer = 10
+    inner = change()
+fo = "br"
+ff = "bb"
