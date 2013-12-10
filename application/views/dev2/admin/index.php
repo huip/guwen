c9babee700dc8b44880ccaee53894cf1bffc1fe1
chinahgr <polandeme@gmail.com>
2013-12-10 14:20:56 +0800

    add admin.css

--- application/views/dev2/admin/index.php
+++ application/views/dev2/admin/index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+<title> 故问后台管理</title>
 <link rel="stylesheet" href="<?php echo base_url(); ?>/data/stylesheets/lib/bootstrap.min.css"  type="text/css" />
 <link rel="stylesheet" href="<?php echo base_url(); ?>/data/css/conn-min.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>/data/stylesheets/admin.css" type="text/css" />
 </head>
 <body>
 <div class = "navbar">
@@ -14,17 +17,16 @@
         </ul>
     </div>
 </div>
-<!-- <script src="<?php echo base_url();?>data/javascripts/sea&#45;modules/seajs/seajs/2.1.1/sea.js"></script> -->
-<!-- <script> -->
-<!-- seajs.config({ -->
-<!--     base: "<?php echo base_url();?>data/javascripts/sea&#45;modules/", -->
-<!--     alias: { -->
-<!--         "$": "jquery/jquery/1.10.1/jquery.js" -->
-<!--     } -->
-<!-- }); -->
-<!-- seajs.use("<?php echo base_url();?>data/javascripts/admin/admin"); -->
-<!-- </script> -->
-<!-- <script src="</script> -->
+<script src="<?php echo base_url();?>data/javascripts/sea&#45;modules/seajs/seajs/2.1.1/sea.js"></script>
+<!-- <script>
+seajs.config({
+    base: "<?php echo base_url();?>data/javascripts/sea-modules/",
+    alias: {
+        "$": "jquery/jquery/1.10.1/jquery.js"
+    }
+});
+seajs.use("<?php echo base_url();?>data/javascripts/admin/admin");
+</script> -->
 </body>
 </html>
 
